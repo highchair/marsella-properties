dc56276e0ccef8eece9b42b9e9657bf33edadd89
jhogue <highchairdesignhaus@gmail.com>
2020-02-15 17:23:45 -0500

    Images, CSS, JS assets

---
+++ assets/js/project.js
@@ -0,0 +1,36 @@
+// Any project-specific code — listeners and triggers for plugins
+
+$(document).ready(function() {
+
+  // Animate some scrolling for smoother transitions 
+  $(function() {
+    $('.js-smoothscroll').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+        }
+      }
+    });
+  });
+  
+  // Animation variable set on the body
+  // Set the root
+  var root = $('body');
+  
+  var container = $('#animated');
+  var viewport_height = window.innerHeight;
+  var this_height = container.outerHeight(true);
+  var scroll_height = viewport_height - this_height;
+  
+  root.css('--scroll', + window.pageYOffset / (document.body.offsetHeight - scroll_height));
+  
+  // Scroll events
+  window.addEventListener('scroll',scroll,false);
+  function scroll() {
+    root.css('--scroll', + window.pageYOffset / (document.body.offsetHeight - scroll_height));
+  }
+});
